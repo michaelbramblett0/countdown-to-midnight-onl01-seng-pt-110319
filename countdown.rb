68dc4a0cbdce4753fd4d35c74f8f2586ab02803c
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-14 23:42:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,8 +8,16 @@ while num > 0
 end
 
 def countdown_with_sleep(num_secs)
-  while num_secs > 0 
-  puts "#{seconds_to_midnight} SECOND(S)!"
-  num_secs -= 1 
+    date1 = Time.now + seconds
+  while Time.now < date1
+    t = Time.at(date1.to_i - Time.now.to_i)
+    p t.strftime('%H:%M:%S')
+    sleep 1
+ 
+ 
+ 
+ #while num_secs > 0 
+  #puts "#{seconds_to_midnight} SECOND(S)!"
+  #num_secs -= 1 
  end 
 end
