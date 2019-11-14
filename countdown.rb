986eaa3e3da7b7bd95ae7ae9cf53e2a7cfb27e9b
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-14 23:04:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+def countdown(num)
+while num > 0
+  puts "#{num} SECOND(S)!"
+  num -= 1
+  end 
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs)
+  while num_secs > 5
+  puts "#{seconds_to_midnight} SECOND(S)!"
+  num_secs -= 5
+ end 
 end
