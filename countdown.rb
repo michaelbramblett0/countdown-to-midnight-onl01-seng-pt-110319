1296d9526ac9f22482e43b7f53688108b35cc766
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-14 23:44:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
 
-def countdown
+def countdown(num)
+while num > 0
+  puts "#{num} SECOND(S)!"
+  num -= 1
+  end 
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs)
+   date1 = Time.now + 5 # your time here
+while Time.now < date1
+  t = Time.at(date1.to_i - Time.now.to_i)
+  p t.strftime('%H:%M:%S')
+  sleep 1
+ 
+ 
+ 
+ #while num_secs > 0 
+  #puts "#{seconds_to_midnight} SECOND(S)!"
+  #num_secs -= 1 
+ end 
 end
